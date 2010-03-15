3a2437fb39f6d9bc013ae40b1025e6826dcdfb64
Will McCutchen <will@keyingredient.com>
2010-03-15 18:08:51 -0500

    Create a new User object for every new log in via Twitter

--- oauth.py
+++ oauth.py
@@ -5,6 +5,7 @@ import tornado.web
 import tweepy
 
 import settings
+from models import User
 
 
 class LoginHandler(tornado.web.RequestHandler):
@@ -44,5 +45,15 @@ class CallbackHandler(tornado.web.RequestHandler):
         # use them for Twitter auth (if they're still valid)
         self.set_secure_cookie('access_token_key', access_token.key)
         self.set_secure_cookie('access_token_secret', access_token.secret)
+
+        # Make sure we have an entity for the user that just logged in
+        api = tweepy.API(auth)
+        user = api.me()
+        if User.get_by_key_name(str(user.id)) is None:
+            logging.info('Creating new User object for %s (%d)' % (
+                    user.screen_name, user.id))
+            user = User(key_name=str(user.id), id=user.id,
+                        screen_name=user.screen_name)
+            user.put()
 
         return self.redirect('/')
