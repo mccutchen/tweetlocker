7309131c0319dbd9c30825d28921a37fa0e55b48
Will McCutchen <will@keyingredient.com>
2010-03-15 02:01:09 -0500

    Store the access token key and secret in cookies when the user is successfully authenticated by Twitter

--- oauth.py
+++ oauth.py
@@ -37,6 +37,9 @@ class CallbackHandler(tornado.web.RequestHandler):
         auth.set_request_token(token_key, token_secret)
         access_token = auth.get_access_token(verifier)
 
-        api = tweepy.API(auth)
-        me = api.me()
-        self.render('templates/index.html', user=me)
+        # Store the access token key and secret in secure cookies, so we can
+        # use them for Twitter auth (if they're still valid)
+        self.set_secure_cookie('access_token_key', access_token.key)
+        self.set_secure_cookie('access_token_secret', access_token.secret)
+
+        return self.redirect('/')
--- trove.py
+++ trove.py
@@ -1,8 +1,27 @@
-import tornado.web
+import logging
+
 from google.appengine.ext import db
+import tornado.web
+import tweepy
+
+import settings
 
 
 class IndexHandler(tornado.web.RequestHandler):
 
     def get(self):
-        self.render('templates/index.html', user=None)
+        auth = tweepy.OAuthHandler(
+            settings.CONSUMER_KEY, settings.CONSUMER_SECRET,
+            'http://localhost:9999/oauth/callback')
+
+        key = self.get_secure_cookie('access_token_key')
+        secret = self.get_secure_cookie('access_token_secret')
+
+        if key and secret:
+            auth.set_access_token(key, secret)
+            api = tweepy.API(auth)
+            user = api.me()
+        else:
+            user = None
+
+        self.render('templates/index.html', user=user)
