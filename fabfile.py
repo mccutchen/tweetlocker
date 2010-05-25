c3f0ed4e74206644fcf9a8b2e91e9abb4b5333c2
Will McCutchen <mccutchen@gmail.com>
2010-05-25 12:40:11 -0500

    Add a clear_datastore command to the fabfile.

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,12 @@
 from fabric.api import *
 
 def deploy():
-    cmd = 'appcfg.py --no_cookies --email=mccutchen@gmail.com update .'
-    local(cmd, capture=False)
+    """Deploys the application to App Engine."""
+    local('appcfg.py --no_cookies --email=mccutchen@gmail.com update .',
+          capture=False)
+
+def clear_datastore():
+    """Clears the production datastore."""
+    local('lib/remote_api_shell.py tweetlocker -p /_/shell -c '
+          '"from lib.utils import clear_datastore; clear_datastore()"',
+          capture=False)
