7755ff6d8ce6883cdd36b2e58b7f42390bb35458
Will McCutchen <wm@bitly.com>
2012-11-14 11:15:00 -0500

    Burn it down

--- lib/env.py
+++
@@ -1,9 +0,0 @@
-"""Consolidate all environment setup functionality (sys.path changes,
-monkeypatches, etc.)  in here."""
-
-import os, sys
-
-# Add our external dependencies to sys.path
-extlibs = ('jinja2', 'tweepy', 'python-simplejson', 'appengine-search')
-for lib in extlibs:
-    sys.path.insert(0, os.path.join('ext', lib))
