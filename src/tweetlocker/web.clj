11f980baeb41d5beb35cca42d637ddf67a9bce54
Will McCutchen <wm@bitly.com>
2013-02-05 15:57:16 -0500

    Add simple request logging

--- src/tweetlocker/web.clj
+++ src/tweetlocker/web.clj
@@ -1,23 +1,47 @@
 (ns tweetlocker.web
-  (:use compojure.core
+  (:use [clojure.pprint :only (pprint)]
+        compojure.core
         [hiccup.middleware :only (wrap-base-url)]
         [hiccup core page])
-  (:require [compojure.route :as route]
+  (:require [clojure.tools.logging :as log]
+            [compojure.route :as route]
             [compojure.handler :as handler]))
 
-(defn index-page []
+(defn wrap-request-logging [app]
+  (fn [{:keys [request-method uri] :as req}]
+    (log/debug req)
+    (let [start (System/currentTimeMillis)
+          resp (app req)
+          finish (System/currentTimeMillis)
+          time (- finish start)
+          status (:status resp)
+          method (.toUpperCase (name request-method))]
+      (log/info (format "%d %s %s %dms" status method uri time))
+      resp)))
+
+(defn render-page
+  [title & body]
   (html5
     [:head
-      [:title "Tweetlocker!"]
-      (include-css "/css/style.css")]
+     [:title title]
+     (include-css "/css/style.css")]
     [:body
-      [:h1 "Tweetlocker!"]]))
+     [:h1 title]
+     body]))
+
+(defn index-page
+  [request]
+  (render-page
+   "Test Page"
+   [:a {:href "/oauth"} "OAuth"]
+   [:pre (with-out-str (pprint request))]))
 
 (defroutes app-routes
-  (GET "/" [] (index-page))
+  (GET "/" request (index-page request))
   (route/resources "/static")
   (route/not-found "Page not found"))
 
 (def app
   (-> (handler/site app-routes)
-      (wrap-base-url)))
+      (wrap-base-url)
+      (wrap-request-logging)))
